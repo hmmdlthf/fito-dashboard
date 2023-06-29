cdbe14d054a21e4b3f03a9e52318d40cc309b010
kevin_jorn <Althaf>
2023-06-29 23:27:21 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function createVLines(no_c, table_lines_group, height, line_gap) {
+    var x1 = 0
+    var y1 = 0
+    var x2 = 0
+    var y2 = height
+
+    for (let i = 0; i < no_c ; i++) {
+        var line = `<line x1=${x1} x2=${x2} y1=${y1} y2=${y2} id='v_line_${i + 1}' stroke-width='2' style='stroke: var(--color-grey-1);' />`;
+
+        table_lines_group.innerHTML += line;
+        x1 += line_gap
+        x2 += line_gap
+    }
+}
+
+function createHLines(no_c, table_lines_group, width, line_gap) {
+    var x1 = 0
+    var y1 = 0
+    var x2 = width
+    var y2 = 0
+
+    for (let i = 0; i < no_c ; i++) {
+        var line = `<line x1=${x1} x2=${x2} y1=${y1} y2=${y2} id='h_line_${i + 1}' stroke-width='2' style='stroke: var(--color-grey-1);' />`;
+
+        table_lines_group.innerHTML += line;
+        y1 += line_gap
+        y2 += line_gap
+    }
+}
---
+++ js/validations.js
@@ -0,0 +1,103 @@
+function showValidationMessage(element_id) {
+    var element = document.getElementById(element_id)
+
+    if (document.getElementById(`${element_id}-error`)) {
+        return
+    }
+
+    var error_label = document.createElement(`label`)
+    error_label.id = `${element_id}-error`
+    error_label.className = `error`
+    error_label.setAttribute('for', `${element_id}`)
+    element.parentElement.appendChild(error_label)
+    error_label.innerHTML = element.validationMessage;
+    document.getElementById(`${element_id}`).classList.add('invalid')
+}
+
+function hideValidationMessage(element_id) {
+
+    if (!document.getElementById(`${element_id}-error`)) {
+        return
+    }
+
+    document.getElementById(`${element_id}-error`).remove()
+    document.getElementById(`${element_id}`).classList.remove('invalid')
+}
+
+function check(fields, location) {
+    var formValid = true
+    fields.forEach(x => {
+        var element1 = document.getElementById(`${x}`)
+
+        if (!element1.checkValidity()) {
+            showValidationMessage(`${x}`)
+            formValid = false
+        }
+    });
+
+    if (formValid) {
+        document.location = location
+    }
+}
+
+function check2(fields, location) {
+    var formValid = true
+    fields.forEach(x => {
+        var element1 = document.getElementById(`${x}_s`)
+        var element2 = document.getElementById(`${x}_e`)
+
+        if (!element1.checkValidity()) {
+            showValidationMessage(`${x}_s`)
+            formValid = false
+        }
+
+        if (!element2.checkValidity()) {
+            showValidationMessage(`${x}_e`)
+            formValid = false
+        }
+    });
+
+    if (formValid) {
+        document.location = location
+    }
+}
+
+function singleFieldCheck(fields) {
+    fields.forEach((x) => {
+        var element1 = document.getElementById(`${x}`)
+
+        element1.addEventListener('blur', () => {
+            if (!element1.checkValidity()) {
+                showValidationMessage(`${x}`)
+            } else {
+                hideValidationMessage(`${x}`)
+            }
+
+        })
+    })
+}
+
+function singleFieldCheck2(fields) {
+    fields.forEach((x) => {
+        var element1 = document.getElementById(`${x}_s`)
+        var element2 = document.getElementById(`${x}_e`)
+
+        element1.addEventListener('blur', () => {
+            if (!element1.checkValidity()) {
+                showValidationMessage(`${x}_s`)
+            } else {
+                hideValidationMessage(`${x}_s`)
+            }
+
+        })
+
+        element2.addEventListener('blur', () => {
+            if (!element2.checkValidity()) {
+                showValidationMessage(`${x}_e`)
+            } else {
+                hideValidationMessage(`${x}_e`)
+            }
+
+        })
+    })
+}
