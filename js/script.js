b6f1a2dd61b47e03daddcb962b6f828d51611c67
kevin_jorn <Althaf>
2023-06-30 01:50:55 +0530

    create svg chart 1

--- js/script.js
+++ js/script.js
@@ -27,3 +27,31 @@ function createHLines(no_c, table_lines_group, width, line_gap) {
         y2 += line_gap
     }
 }
+
+function createline1Dots(no_c, table_lines_group, height, line_gap, values, color) {
+    var x1 = 0
+
+    for (let i = 0; i < no_c; i++) {
+        var y1 = (height * 4 / 5) * values[i] / 100
+        var circle = `<circle cx=${x1} cy=${y1} r=${5} fill="#fff" id='line1_circle_${i + 1}' stroke-width='4' style='stroke: ${color};' />`;
+
+        table_lines_group.innerHTML += circle;
+        x1 += line_gap
+    }
+}
+
+function createline1lines(no_c, table_lines_group, height, line_gap, values, color) {
+    var x1 = 0
+    var x2 = line_gap
+
+    for (let i = 0; i < no_c - 1; i++) {
+        var y1 = (height * 4 / 5) * values[i] / 100
+        var y2 = (height * 4 / 5) * values[i + 1] / 100
+        var line = `<line x1=${x1} x2=${x2} y1=${y1} y2=${y2} id='line1_line_${i + 1}' stroke-width='4' style='stroke: ${color};' />`;
+
+
+        table_lines_group.innerHTML += line;
+        x1 += line_gap
+        x2 += line_gap
+    }
+}
